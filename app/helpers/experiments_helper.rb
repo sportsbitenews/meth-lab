6f7a43deb3790552c492c5913d43f5d9fb31eda3
Shai Wininger <shai.wininger@gmail.com>
2013-09-17 00:10:32 +0300

    display low deterministic results in gray

--- app/helpers/experiments_helper.rb
+++ app/helpers/experiments_helper.rb
@@ -5,9 +5,12 @@ module ExperimentsHelper
 	end
 	
 
-	def class_for_goal_row(performance_percent)
+	def class_for_goal_row(experiment, kpi)
+			performance_percent = experiment.performance_perc(kpi)
+			total_goals = experiment.control_kpis[kpi].to_i + experiment.experiment_kpis[kpi].to_i
+
 			return if performance_percent.nil?
-			return 'disabled' if performance_percent > -3 && performance_percent < 3
+			return 'disabled' if (total_goals < 500) || performance_percent > -3 && performance_percent < 3
 			return 'success' if performance_percent > 3
 			return 'warning' if performance_percent <= -3 && performance_percent > -6
 			return 'error' if performance_percent <= -6
